56f9cd092bf63fac1851f85d765bac7d69074778
ToxicTrigger <syoa1996@gmail.com>
2019-01-24 14:26:47 +0900

    :arrow_up: Edit main.rs more stringent experimental code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+///TODO ::  Math Equation Parser
+///TODO :: Parm Getter
+///TODO :: Sqllite DB
+///TODO :: Daemon service
+// Date - 19/01/24 ToxicTrigger
+
 use std::collections::BTreeMap;
 
 /// # EquationMap
@@ -12,9 +18,10 @@ impl EquationMap
 {
     pub fn new() -> EquationMap
     {
-        return EquationMap{equations : BTreeMap::new() };
+        return EquationMap{ equations : BTreeMap::new() };
     }
 
+    /// - return -> T = Success , F = Failed
     pub fn insert(&mut self, equation_name : String, equation : Equation)->bool
     {
         let op = self.equations.insert(equation_name, equation);
@@ -35,10 +42,25 @@ struct Equation
     value : String
 }
 
-
 fn main()
 {
     let mut maps = EquationMap::new();
-    maps.insert("Test001".to_string(), Equation{value:"x+y".to_string()});
-    println!("value = {:?}" , maps.equations.get("Test001").unwrap());
+    if maps.insert("Add".to_string(), Equation{value:"x+y".to_string()})
+    {
+        let mut equ = String::new();
+        println!("Typing Equation Name!");
+        std::io::stdin().read_line(&mut equ).unwrap();
+
+        // Delete \n
+        if let Some('\n') = equ.chars().next_back()
+        {
+            equ.pop();
+        }
+
+        match maps.equations.get(&equ)
+        {
+            Some(s) => println!("{:?}" , s),
+            None => println!("{} is null",equ),
+        }
+    }
 }
